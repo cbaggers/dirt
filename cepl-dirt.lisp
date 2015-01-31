a1f10e24e848002ec00b4398b07fba3ad7b907ed
Baggers <chris.bagley@gmail.com>
2015-01-31 12:03:03 +0100

    fix issues with texture formats and cepl

--- cepl-dirt.lisp
+++ cepl-dirt.lisp
@@ -7,25 +7,32 @@
       t
       (error "Format must be either :rgb, :rgba or :auto~%Given ~s" format)))
 
-(defun get-element-type (format)
+(defun dirt-format-to-cepl-format (format)
   (case format
     (:rgb :ubyte-vec3)
     (:rgba :ubyte-vec4)))
+(defun dirt-format-to-texture-internal (format)
+  (case format
+    (:rgb :rgb8)
+    (:rgba :rgba8)))
 
 (defun load-image (filepath &optional (format :rgba))
   "Load image from disk to c-array"
   (when (check-formats format)
     (destructuring-bind (pointer width height channels) 
         (cl-soil:load-image filepath format)
-      (values (cgl:make-c-array-from-pointer (list width height) 
-                                             (get-element-type format)
-                                             pointer)
+      (values (cgl:make-c-array-from-pointer
+               (list width height)
+               (dirt-format-to-cepl-format format)
+               pointer)
               channels))))
 
 (defun load-image-to-texture (filepath &optional texture (format :rgba))
-  (let ((data (load-image filepath format)))
+  (let ((data (load-image filepath format))
+        (internal-format (dirt-format-to-texture-internal format)))
     (unwind-protect 
-         (cond ((typep texture 'null) (cgl:make-texture :initial-contents data))
+         (cond ((typep texture 'null) (cgl:make-texture 
+                                       data :internal-format internal-format))
                ((typep texture 'cgl:gl-texture) (cgl:gl-push 
                                                  (cgl:texref texture) data))
                ((typep texture 'cgl:gpu-array-t) (cgl:gl-push texture data)))
