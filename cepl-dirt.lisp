3d439190887bf18b77be6f392e71b5b083e3c8d4
baggers <baggers@cave>
2016-04-16 11:48:32 +0200

    update to use int8 types

--- cepl-dirt.lisp
+++ cepl-dirt.lisp
@@ -9,8 +9,8 @@
 
 (defun dirt-format-to-cepl-format (format)
   (case format
-    (:rgb :ubyte-vec3)
-    (:rgba :ubyte-vec4)))
+    (:rgb :uint8-vec3)
+    (:rgba :uint8-vec4)))
 (defun dirt-format-to-texture-internal (format)
   (case format
     (:rgb :rgb8)
@@ -19,7 +19,7 @@
 (defun load-image (filepath &optional (format :rgba))
   "Load image from disk to c-array"
   (when (check-formats format)
-    (destructuring-bind (pointer width height channels) 
+    (destructuring-bind (pointer width height channels)
         (cl-soil:load-image filepath format)
       (values (cgl:make-c-array-from-pointer
                (list width height)
@@ -30,10 +30,10 @@
 (defun load-image-to-texture (filepath &optional texture (format :rgba))
   (let ((data (load-image filepath format))
         (internal-format (dirt-format-to-texture-internal format)))
-    (unwind-protect 
-         (cond ((typep texture 'null) (cgl:make-texture 
+    (unwind-protect
+         (cond ((typep texture 'null) (cgl:make-texture
                                        data :internal-format internal-format))
-               ((typep texture 'cgl:gl-texture) (cgl:gl-push 
+               ((typep texture 'cgl:gl-texture) (cgl:gl-push
                                                  (cgl:texref texture) data))
                ((typep texture 'cgl:gpu-array-t) (cgl:gl-push texture data)))
       (cgl:free-c-array data))))
