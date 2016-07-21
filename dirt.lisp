8118a3fe645c4605c01fe8357798026b27d8f1a3
Baggers <chris.bagley@gmail.com>
2016-07-21 20:20:03 +0200

    make use of cl-soils 'actual component count'
    Before it would return channel count, but this was the count from the
    file and not reflect whether you had forced-channels or not.
    
    So for example if you loaded an image in :rgb format but forced to
    :rgba, the component-length would be 3, even though the loaded data
    contained 4 components.

--- dirt.lisp
+++ dirt.lisp
@@ -2,21 +2,24 @@
 
 (in-package #:dirt)
 
-(defun load-image-to-c-array (filename)
-  (multiple-value-bind (ptr width height components)
-      (cl-soil:load-image filename)
-    (let ((elem-type (nth (1- components) '(nil nil :uint8-vec3 :uint8-vec4))))
-      (cepl:make-c-array-from-pointer (list width height) elem-type ptr))))
+(defun load-image-to-c-array (filename &optional (image-format :rgba))
+  (let ((filename (if (pathnamep filename)
+		      (namestring filename)
+		      filename)))
+    (multiple-value-bind (ptr width height components actual-components)
+	(cl-soil:load-image filename image-format)
+      (declare (ignore components))
+      (let ((elem-type (nth (1- actual-components)
+			    '(nil nil :uint8-vec3 :uint8-vec4))))
+	(cepl:make-c-array-from-pointer (list width height) elem-type ptr)))))
 
 
 (defun load-image-to-texture (filename &optional (image-format :rgba8) mipmap
 					 generate-mipmaps)
-  (let* ((array (load-image-to-c-array filename))
-	 (pixel-format (cepl:lisp-type->pixel-format :uint8-vec4))
+  (let* ((array (load-image-to-c-array filename :rgba))
 	 (texture (cepl:make-texture
 		   array
 		   :element-type image-format
-		   :pixel-format pixel-format
 		   :mipmap mipmap
 		   :generate-mipmaps generate-mipmaps)))
     (cepl:free-c-array array)
