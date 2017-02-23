4a564c936ba5a76f18f290900188d66415fe89f1
baggers <baggers@cave>
2017-02-23 19:04:01 +0100

    add save-as-image
    and fix whitespace

--- dirt.lisp
+++ dirt.lisp
@@ -2,25 +2,50 @@
 
 (in-package #:dirt)
 
+(defvar *valid-save-image-formats*
+  '(:tga :bmp :dds))
+
 (defun load-image-to-c-array (filename &optional (image-format :rgba))
   (let ((filename (if (pathnamep filename)
-		      (namestring filename)
-		      filename)))
+                      (namestring filename)
+                      filename)))
     (multiple-value-bind (ptr width height components actual-components)
-	(cl-soil:load-image filename image-format)
+        (cl-soil:load-image filename image-format)
       (declare (ignore components))
       (let ((elem-type (nth (1- actual-components)
-			    '(nil nil :uint8-vec3 :uint8-vec4))))
-	(cepl:make-c-array-from-pointer (list width height) elem-type ptr)))))
-
+                            '(nil nil :uint8-vec3 :uint8-vec4))))
+        (cepl:make-c-array-from-pointer (list width height) elem-type ptr)))))
 
 (defun load-image-to-texture (filename &optional (image-format :rgba8) mipmap
-					 generate-mipmaps)
+                                         generate-mipmaps)
   (let* ((array (load-image-to-c-array filename :rgba))
-	 (texture (cepl:make-texture
-		   array
-		   :element-type image-format
-		   :mipmap mipmap
-		   :generate-mipmaps generate-mipmaps)))
+         (texture (cepl:make-texture
+                   array
+                   :element-type image-format
+                   :mipmap mipmap
+                   :generate-mipmaps generate-mipmaps)))
     (cepl:free-c-array array)
     texture))
+
+(defgeneric save-as-image (texture file-path &optional format)
+  ;;
+  (:method ((texture cepl:texture) file-path &optional format)
+           (cepl:with-c-array (arr (cepl:pull1-g texture))
+             (save-as-image arr file-path format)))
+  ;;
+  (:method ((image cepl:c-array) file-path &optional format)
+    (labels ((path->fmt (path)
+               (let ((tmp (pathname-type path)))
+                 (when tmp (intern (string-upcase tmp) :keyword)))))
+      (let* ((path (uiop:coerce-pathname file-path))
+             (format (or format (path->fmt path))))
+        (assert (member format *valid-save-image-formats*) (format)
+                "Dirt: save-as-image can only save in the following formats:~%~a~%~%However the format ~a was requested"
+                *valid-save-image-formats* format)
+        (destructuring-bind (x y) (cepl:c-array-dimensions image)
+          (let* ((elem-type (cepl:c-array-element-type image))
+                 (pixel-fmt (cepl:lisp-type->pixel-format elem-type))
+                 (component-len (cepl:pixel-format-comp-length pixel-fmt)))
+            (cl-soil:save-image file-path format x y component-len
+                                (cepl:c-array-pointer image))
+            file-path))))))
--- package.lisp
+++ package.lisp
@@ -3,4 +3,5 @@
 (defpackage #:dirt
   (:use #:cl)
   (:export :load-image-to-c-array
-           :load-image-to-texture))
+           :load-image-to-texture
+           :save-as-image))
